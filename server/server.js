6ca2ef81ae7fd1a36b1db809277ec0d7606e23a3
tavoGeek <gcnctz@gmail.com>
2020-05-16 18:18:10 -0500

    gcn

--- server/server.js
+++ server/server.js
@@ -8,13 +8,35 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get("/usuarios", function (req, res) {
-  res.json("Get Usuarios");
+app.get("/usuario", function (req, res) {
+  res.json("get Usuario");
 });
 
-app.post("/usuarios", function (req, res) {
-    let body = req.body;
-    res.json({body});
+app.post("/usuario", function (req, res) {
+  let body = req.body;
+
+  if (body.nombre === undefined) {
+    res.status(400).json({
+      ok: false,
+      mensaje: "El nombre es necesario",
+    });
+  } else {
+    res.json({
+      persona: body,
+    });
+  }
+});
+
+app.put("/usuario/:id", function (req, res) {
+  let id = req.params.id;
+
+  res.json({
+    id,
+  });
+});
+
+app.delete("/usuario", function (req, res) {
+  res.json("delete Usuario");
 });
 
 app.listen(process.env.PORT, () => {
